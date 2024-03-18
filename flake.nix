d0d95cb8223d19716c337d8196934856d38c2eb4
Robert Krahn <robert.krahn@gmail.com>
2024-03-18 19:37:27 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkgs.rustc
+              pkgs.cargo
+              clang
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              openssl
+            ];
+
+            packages = with pkgs; [
+              rust-analyzer
+              yt-dlp
+            ];
+
+            RUST_BACKTRACE = "1";
+            RUST_LOG = "debug";
+          };
+        }
+    );
+}
